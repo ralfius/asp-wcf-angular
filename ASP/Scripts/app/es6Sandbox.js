75f01c6766c82f239a986531a1ef505277ca5e35
Vladimir Ivanov <vl.ivanov90@gmail.com>
2016-07-14 17:19:55 +0300

    Added EcmaScript6 playground file

--- ASP/Scripts/app/es6Sandbox.js
+++ ASP/Scripts/app/es6Sandbox.js
@@ -20,7 +20,7 @@ let arr = [1, 2, 3];
 let doubledArr = Array.from([1, 2, 3], doubleFunction);
 
 
-//Distructuring
+//Destructuring
 arr = [1, 2, 3];
 let ob = { d: 1, e: 2 };
 
@@ -30,6 +30,3 @@ let { d, e } = ob;
 
 let { d: d2, e: e2 } = { d, e };
 //d2 and e2 are now local variables coppied {d, e}
-
-
-let debug = 10;
