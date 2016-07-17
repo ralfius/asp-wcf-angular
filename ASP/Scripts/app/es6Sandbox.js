45957285a3a17edacd851db43ee334faff98a97a
Vladimir Ivanov <vl.ivanov90@gmail.com>
2016-07-17 12:13:30 +0300

    Added details to ES6 sandbox

--- ASP/Scripts/app/es6Sandbox.js
+++ ASP/Scripts/app/es6Sandbox.js
@@ -13,12 +13,16 @@ function getValues(strings, ...values){
 let values = getValues `some string with ${firstName} and ${lastName}`;
 
 // lambda
-let doubleFunction = item => item * 2; //'this' is captured
+let doubleFunction = item => item * 2; 
+let getThis = () => this; //'this' is captured
+let thisEQ = this === getThis();
+let getArguments = (function() {
+    return (x, y) => arguments; //no arguments (taken from outside)
+}());
 
 // arrays
 let arr = [1, 2, 3];
 let doubledArr = Array.from([1, 2, 3], doubleFunction);
-
 
 // destructuring
 arr = [1, 2, 3];
@@ -29,6 +33,21 @@ let { d, e } = ob; //a, b, c, d, e are now local variables coppied from array an
 
 
 let { d: d2, e: e2 } = { d, e }; //d2 and e2 are now local variables coppied {d, e}
+
+function rectArea({width: w = 0, height: h = 0} = {}){
+    return h * w;
+}
+
+let ra1 = rectArea({width: 10, height: 20});
+let ra2 = rectArea({height: 10});
+let ra3 = rectArea();
+
+// function name
+function myFunc1(){}
+let myFunc2 = function(){}
+
+let func1Name = myFunc1.name;
+let func2Name = myFunc2.name;
 
 // default parameters
 //function modulo(value, deliminer = 2){
@@ -46,6 +65,12 @@ function sum(value, ...values){
     return sum;
 }
 
+// function visibility
+//{
+//    function BlockFunc(){}
+//}
+//BlockFunc(); //error
+
 // spread
 let arr1 = [3, 4, 5];
 let arr2 = [1, 2, ...arr1];
@@ -53,20 +78,20 @@ let max = Math.max(...arr2); // invokes Math.max(1,2,3,4,5);
 
 // for-of loop
 let forOfResult = [];
-for (let i of [1, 2, 3]) {
+for (var i of [1, 2, 3]) {
     forOfResult.push(i);
 }
 
 // generators
 function* generator(obj) {
-    for (let key in obj) {
+    for (var key in obj) {
         yield [key, obj[key]]
     }
 }
 
 let generatorResult = [];
 
-for (let [key, value] of generator({a:5, b:10})) {
+for (var [key, value] of generator({a:5, b:10})) {
     generatorResult.push([key, value]);
 }
 
@@ -74,6 +99,6 @@ Object.defineProperty(Object.prototype, Symbol.iterator, {
     value: generator
 });
 
-for (let [key, value] of {c:25, d:50}) {
+for (var [key, value] of {c:25, d:50}) {
     generatorResult.push([key, value]);
 }
