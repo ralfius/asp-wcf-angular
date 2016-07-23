ff6abc5c2d5d443d32cebc876230cf8b05854bce
Vladimir Ivanov <vl.ivanov90@gmail.com>
2016-07-23 18:42:48 +0300

    Scripts folder is reorganized

--- ASP/App_Start/BundleConfig.cs
+++ ASP/App_Start/BundleConfig.cs
@@ -10,20 +10,20 @@ namespace ASP
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.js"));
+                "~/Scripts/lib/jquery/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                "~/Scripts/jquery.validate*"));
+                "~/Scripts/lib/jquery/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                "~/Scripts/modernizr-*"));
+                "~/Scripts/lib/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Scripts/bootstrap.js",
-                "~/Scripts/respond.js",
-                "~/Scripts/ui-bootstrap-tpls-1.3.3.js"));
+                "~/Scripts/lib/bootstrap.js",
+                "~/Scripts/lib/respond.js",
+                "~/Scripts/lib/ui-bootstrap-tpls-1.3.3.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                 "~/Content/bootstrap.css",
@@ -35,7 +35,7 @@ namespace ASP
                 "~/Scripts/app/es6Sandbox.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/angular").Include(
-                "~/Scripts/angular.js"));
+                "~/Scripts/lib/angular/angular.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/app").Include(
                 "~/Scripts/app/helpers.js",
